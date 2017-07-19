c92769906dece702fec172cf560342d4c8fc291d
Sam Headlee <sam.headlee@concordusa.com>
2017-07-19 16:59:12 -0500

    xxxxx

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println("\n\nDynamic Port in Test case-1: " + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
-    }
+    } 
     
     @Override
     protected String[] getConfigFiles() {
---
+++ src/test/java/com/mulesoft/training/Rule.java
@@ -0,0 +1,5 @@
+package com.mulesoft.training;
+
+public @interface Rule {
+
+}
